7c0a3a50f54d4db7640edb62b857b5935a2247a5
ljg294 <ljg29494@gmail.com>
2024-09-03 12:57:11 +0900

    _config.yml modified

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725246171',resources: [
+    cacheName: 'chirpy-1725247165',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
