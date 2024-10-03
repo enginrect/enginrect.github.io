a8784c280bb605efdd4c3ef74610909c3a266ffb
ljg294 <ljg29494@gmail.com>
2024-10-03 20:40:23 +0900

    fit dine architecture modified

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1727955235',resources: [
+    cacheName: 'chirpy-1727955610',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
