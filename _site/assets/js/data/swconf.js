79a50b03e307abea2ab3ffc4e368b59c6aec1b79
ljg294 <ljg29494@gmail.com>
2024-10-07 18:27:48 +0900

    url changed

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728287264',resources: [
+    cacheName: 'chirpy-1728293246',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
