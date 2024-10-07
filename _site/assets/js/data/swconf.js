e2a7ead72ea3b02200c5a7a0eb8afcdd9eba7af7
ljg294 <ljg29494@gmail.com>
2024-10-07 16:48:06 +0900

    font changed

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728286028',resources: [
+    cacheName: 'chirpy-1728287264',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
