21bdedb94e73a12041605643b841bbf86290cbd4
ljg294 <ljg29494@gmail.com>
2024-10-23 22:53:00 +0900

    mermaid chart modified

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728293246',resources: [
+    cacheName: 'chirpy-1729691557',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
