6344e81efe4d56f09ccd9e7af1c0aa08b1d1ffe7
ljg294 <ljg29494@gmail.com>
2024-12-03 12:27:00 +0900

    .gitignore modified

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729691557',resources: [
+    cacheName: 'chirpy-1733560333',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
